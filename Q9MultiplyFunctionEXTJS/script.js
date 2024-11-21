2315ee98a6625f41e4c31fbf6587045162c9e734
Prathamesh Charoskar <145451768+pratham2060@users.noreply.github.com>
2024-11-21 20:06:07 +0530

    Add files via upload

---
+++ Q9MultiplyFunctionEXTJS/script.js
@@ -0,0 +1,6 @@
+function mul(a,b,c,d)
+{
+ console.log("Ans of "+a+" * "+b+" = "+(a*b));
+ console.log("Ans of "+a+" * "+b+" * "+c+" = "+(a*b*c));
+ console.log("Ans of "+a+" * "+b+" * "+c+" * "+d+" = "+(a*b*c*d));
+}
